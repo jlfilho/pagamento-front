7c3d14aeb22d72797b4d3027314f283f7892a820
João da Mata <jlfilho@uea.edu.br>
2023-07-28 18:39:45 -0400

    Versão 0.0.6: Criado o módulo lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,38 +13,32 @@ import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatTooltipModule} from '@angular/material/tooltip';
 
 
-
-
-
-
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+
 import { PessoaPesquisaComponent } from './pessoa-pesquisa/pessoa-pesquisa.component';
-import { LancamentoPesquisaComponent } from './lancamento-pesquisa/lancamento-pesquisa.component';
-import { LancamentoGridComponent } from './lancamento-grid/lancamento-grid.component';
 import { PassoaGridComponent } from './passoa-grid/passoa-grid.component';
+import { LancamentosModule } from './lancamentos/lancamentos.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     PessoaCadastroComponent,
     NavbarComponent,
-    LancamentoCadastroComponent,
     PessoaPesquisaComponent,
-    LancamentoPesquisaComponent,
-    LancamentoGridComponent,
+
     PassoaGridComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+
+    LancamentosModule,
+
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
---
+++ src/app/lancamentos/lancamentos.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
+
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentoPesquisaComponent } from './lancamento-pesquisa/lancamento-pesquisa.component';
+import { LancamentoGridComponent } from './lancamento-grid/lancamento-grid.component';
+
+@NgModule({
+  declarations: [
+    LancamentoCadastroComponent,
+    LancamentoPesquisaComponent,
+    LancamentoGridComponent
+  ],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatTooltipModule
+  ],
+  exports: [
+    LancamentoCadastroComponent,
+    LancamentoPesquisaComponent
+  ]
+})
+export class LancamentosModule { }
