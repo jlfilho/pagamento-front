7a06010df88b980aa336bd6a45556ea0e057122c
João da Mata <jlfilho@uea.edu.br>
2023-07-28 19:04:19 -0400

    Versão 0.0.9: Criado as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LancamentoPesquisaComponent } from './lancamentos/lancamento-pesquisa/lancamento-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import { PessoaPesquisaComponent } from './pessoas/pessoa-pesquisa/pessoa-pesquisa.component';
+import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: LancamentoPesquisaComponent},
+  {path: "lancamento/novo", component: LancamentoCadastroComponent},
+  {path: "pessoa", component: PessoaPesquisaComponent},
+  {path: "pessoa/nova", component: PessoaCadastroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
