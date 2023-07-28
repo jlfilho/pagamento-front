6e432c9aebec1bca548b0b4ddca8bef8bfa2a095
João da Mata <jlfilho@uea.edu.br>
2023-07-28 19:18:42 -0400

    Versão 0.0.10: Criado menu lateral (sidebar)

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,8 +4,12 @@ import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -14,9 +18,15 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     CommonModule,
+    RouterModule,
+
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatDividerModule
+
   ],
   exports: [
     NavbarComponent
